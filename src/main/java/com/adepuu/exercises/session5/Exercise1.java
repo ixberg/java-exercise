5ec023a0b9c4adb4413ceedeecda21466093e3e1
Muh Reyzki <muhreyzki11@gmail.com>
2024-03-25 10:07:39 +0700

    submit exercise1

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,17 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise1 {
+    public static void main(String[] args) {
+        int[] myNum = {8, 2, 13, 1, 4} ;
+        int max = myNum[0] ;
+
+        for (int i = 0; i < myNum.length; i++) {
+            if (max < myNum[i]) {
+                max = myNum[i];
+            }
+        }
+        System.out.println(max);
+
+
+    }
 }
