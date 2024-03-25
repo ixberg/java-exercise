6577deb5a0f46be10a93dbf2c311baf9d43ac4d7
Muh Reyzki <muhreyzki11@gmail.com>
2024-03-25 15:20:34 +0700

    submit exercise5

--- src/main/java/com/adepuu/exercises/session5/Exercise5.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise5.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Exercise5 {
     /**
      * Java Array Program to sort array in increasing & decreasing order
@@ -13,6 +16,71 @@ public class Exercise5 {
      * Output: [ 8,7, 5, 2 ]
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] inputArray = {8, 7, 5, 2};
+        System.out.println(Arrays.toString(inputArray));
+        System.out.print("Enter direction (asc/desc): ");
+        String direction = scanner.next();
 
+
+        sortArray(inputArray, direction);
+
+        System.out.println("Sorted array in " + direction + " order: " + Arrays.toString(inputArray));
+    }
+
+    public static void sortArray(int[] array, String direction) {
+        switch (direction) {
+            case "asc":
+                Arrays.sort(array);
+                break;
+            case "desc":
+                reverseArray(array);
+                break;
+            default:
+                System.out.println("Invalid Operation");
+                break;
+        }
+    }
+
+    public static void reverseArray(int[] array) {
+        int start = 0;
+        int end = array.length - 1;
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String direction = scanner.next();
+//        int[] inputArray = {8, 7, 5, 2};
+//
+//    }
+//
+//    public static int[] sortArray(int[] array, String direction) {
+//        return switch (direction) {
+//            case "asc" -> Arrays.sort(array);
+//            case "desc" -> reversArray(array);
+//            default -> {
+//                System.out.println("Invalid Operation");
+//                yield 0;
+//            }
+//        };
+//    }
+//
+//    public static void reversArray(int[] array) {
+//        int start = 0;
+//        int end = array.length - 1;
+//        while (start < end) {
+//            int temp = array[start];
+//            array[start] = array[end];
+//            array[end] = temp;
+//            start++;
+//            end--;
+//        }
+//    }
+//}
